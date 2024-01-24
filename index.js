e3c2f24b6de446b13bc6cb044a613dbaa07d50ab
SumanM1234 <sumanmallick121@gmail.com>
2024-01-24 11:43:04 +0530

    Combining reducers

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+const ICECREAM_ORDERED = "ICECREAM_ORDERED"
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED"
 
 function orderCake () // Action creator function
 {
@@ -21,35 +24,84 @@ function restockCake(qty = 1)
     }
 }
 
-// Reducer: (previousState, action) => newState
-
-const initialState = {
-    numOfCakes: 10,
-    anotherProperty: 0,
+function orderIceCream() // Action creator function
+{
+  return {
+    type: ICECREAM_ORDERED, // 'Action' with 'type' property
+    quantity: 1,
+  }
 }
 
-const reducer = (state = initialState, action) => {   // action is orderCake(), restockCake()
+function restockIceCream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  }
+}
+
+
+// Reducer: (previousState, action) => newState
+
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 20,
+// }
+
+const initialCakeState = {
+  numOfCakes: 10,
+}
+
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+}
+
+const cakeReducer = (state = initialCakeState, action) => {   // action is orderCake(), restockCake()
     switch (action.type) {
-        case CAKE_ORDERED:
-            return {
-                ...state,  // making copy of the initialState & updating the required property/properties
-                numOfCakes: state.numOfCakes - 1
-            }
-        case CAKE_RESTOCKED:
-            return {
-                ...state,
-                numOfCakes: state.numOfCakes + action.payload
-            }
-        default:
-            return state
+      case CAKE_ORDERED:
+        return {
+          ...state, // making copy of the initialState & updating the required property/properties
+          numOfCakes: state.numOfCakes - 1,
+        }
+      case CAKE_RESTOCKED:
+        return {
+          ...state,
+          numOfCakes: state.numOfCakes + action.payload,
+        }
+      default:
+        return state
     }
 }
 
-const store = createStore(reducer)   // First line of execution & store is created
-console.log('Initial state : ', store.getState())
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  // action is orderCake(), restockCake()
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
+      };
+    default:
+      return state;
+  }
+}
 
-const unsubscribe = store.subscribe(() =>    // Listener gets called anytime the store updates
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer,
+})
+
+const store = createStore(rootReducer)   // First line of execution & store is created
+console.log('Initial state : ', store.getState())
+// console.log('Initial state : ', store.getState().cake.numOfCakes) // Only accessing numOfCakes property from the rootReducer
+const unsubscribe = store.subscribe(() =>
+  // Listener gets called anytime the store updates
   console.log("Updated state : ", store.getState())
+  // console.log("Updated state : ", store.getState().cake.numOfCakes)
 )
 
 // store.dispatch(orderCake())  // Action is added to the reducer & the state gets updated with the new state
@@ -57,11 +109,14 @@ const unsubscribe = store.subscribe(() =>    // Listener gets called anytime the
 // store.dispatch(orderCake())
 // store.dispatch(restockCake(3))
 
-const actions = bindActionCreators({orderCake, restockCake}, store.dispatch)  
+const actions = bindActionCreators({orderCake, restockCake, orderIceCream, restockIceCream}, store.dispatch)  
 // Provide the list of functions as the fist argument & provide the store dispatch 
 actions.orderCake()
 actions.orderCake()
 actions.orderCake()
 actions.restockCake(3)
+actions.orderIceCream()
+actions.orderIceCream()
+actions.restockIceCream(2)
 
 unsubscribe()  //Listener unsubscribed & after this the store won't get updated
